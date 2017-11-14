dff26369967036dc8d5b107db692005ebad4b1a9
RyleeAndrews <ryleeandrews73@gmail.com>
2017-11-14 11:35:00 -0800

    all tests passing now on to readme and lab is finished

--- test/queueConstructor-test.js
+++ test/queueConstructor-test.js
@@ -10,11 +10,19 @@ describe('queue test', () => {
     let queueTest = new Queue();
     expect(queueTest.length).toBe(0);
   });
-  it('should return 1,2,3', () => {
+  it('should return 1,2,3 for enqueue', () => {
     let queueTest = new Queue();
     queueTest.enqueue(1);
     queueTest.enqueue(3);
     expect(queueTest.length).toBe(2);
     expect(queueTest[1]).toBe(3);
+  });
+  it('should return 1,2,3 for dequeue', () => {
+    let queueTest = new Queue();
+    queueTest.enqueue(1);
+    queueTest.enqueue(3);
+    expect(queueTest.dequeue()).toBe(1);
+    expect(queueTest.dequeue()).toBe(3);
+    expect(queueTest.dequeue()).toBe(undefined);
   });
 });

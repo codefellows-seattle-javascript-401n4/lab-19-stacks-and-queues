583db860cb598a410cfd2e3d74deb39797167b2c
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-11-15 17:06:39 -0800

    lab done

--- queue.test.js
+++ queue.test.js
@@ -1,15 +1,28 @@
+/*global expect */
 'use strict';
 
-module.edports = class Queue {
-    constructor() {
-    	this.queue = [];
-    }
-    
-    unshift(value) {
-    	this.queue.unshift(value);
-    }
-    
-    shift() {
-    	this.queue.shift();
-    }
-}
+let Queue = require('./queue.js');
+
+describe('QUEUE', () => {
+  let queue = new Queue();
+  
+  // unshift tests
+  test('unshift should add a number to beginning of queue', () => {
+    queue.unshift(3);
+    queue.unshift(2);
+    queue.unshift(1);
+    expect(queue.queue[0]).toEqual(1);
+  });
+  test('unshift should increase length by 1 each time', () => {
+    expect(queue.queue.length).toEqual(3);
+  });
+
+  // shift tests
+  test('shift should remove and return the first item in the queue', () => {
+    let expected = queue.shift();
+    expect(expected).toBe(1);
+  });
+  test('shift should reduce length by 1 each time', () => {
+    expect(queue.queue.length).toEqual(2);
+  });
+});

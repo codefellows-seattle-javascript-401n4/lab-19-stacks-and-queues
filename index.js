b06c505afe2f722eaeee1c76f1e03d245edf5c98
Iryna Maslova <maslovai@hotmail.com>
2017-11-15 17:38:55 -0800

    stack const done

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 'use strict';
-const linkedList = require('./lib/queue-constructor');
+// const linkedList = require('./lib/queue-constructor');
+const stackConst = require('./lib/stack-constructor');
 
-let listOne = new linkedList;
+// let listOne = new linkedList;
 
+// console.log("after enqueue ");
+// for(let i = 1; i<4; i++){
+//     listOne.enqueue(i)
+// }
 
-console.log("after enqueue ");
-for(let i = 1; i<4; i++){
-    listOne.enqueue(i)
-}
+// console.log("after dequeue ");  
+// for(let i = 1; i<4; i++){
+//     listOne.dequeue();
+// }
 
-console.log("after dequeue ");  
-for(let i = 1; i<4; i++){
-    listOne.dequeue();
-}
+let stack = new stackConst;
+stack.push(5);
+console.log("after push:", stack);
+console.log("after pop", stack.pop());

7d1fbe83578ff45570889342dffbc810ab4a11fa
Iryna Maslova <maslovai@hotmail.com>
2017-11-16 11:12:57 -0800

    tests for both passing

--- test/queue-constructor.test.js
+++ test/queue-constructor.test.js
@@ -2,15 +2,23 @@
 
 const LL = require('../lib/queue-constructor');
 const expect = require('expect');
+let llTest = new LL();
 
 describe('Linked List', ()=>{
 
-    it('should return a tail of a linked after enqueue', ()=>{
-      let llTest = new LL();
+    it('should return a tail of a linked list after enqueue', ()=>{
+      
       for(let i = 1; i<4; i++){
         llTest.enqueue(i)
-        console.log(llTest);
+        expect(llTest.tail.value).toEqual(i)
     }
-      expect(llTest.tail).not.toBe(null)
+    // console.log('tail in test:', llTest.tail);
+      
+    })
+   
+    it("should remove the head if a list via dequeue",()=>{
+      llTest.dequeue();
+      expect(llTest.head.value).toEqual(2);
+
     })
 })

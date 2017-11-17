a15032d37a1f9bcf5ada0f6c19ae183a04339cc7
dalhun namgung <bnamgung@gmail.com>
2017-11-17 14:05:27 -0800

    still fixing

--- test/queue.test.js
+++ test/queue.test.js
@@ -8,24 +8,44 @@ const expect = require('expect');
 
 
 describe('queue testing', () => {
-    let newQueue = new Queue();
 
-    it('should return object when done', (done) => {
-      newQueue = newQueue.enqueue(1);
-      newQueue = newQueue.enqueue(2);
-      newQueue = newQueue.enqueue(3);
+    it('should return 0 when done', () => {
+      let newQueue = new Queue();
+      expect(newQueue.length).toBe(null);
 
-      expect(newQueue).toEqual([1,2,3]);
-      expect(newQueue.queue[1]).toEqual(2);
+    });
+
+    it('should return 3, 2, null, 1', () => {
+      let newQueue = new Queue();
+
+      newQueue.enqueue(1);
+      newQueue.enqueue(3);
+      expect(newQueue.length).toBe(2);
+      expect(newQueue[1].toBe(3);
       done();
+      
+    });
+
+    it('should return 2,3', () => {
+      let newQueue = new Queue();
+
+      newQueue.enqueue(1);
+      newQueue.enqueue(3);
+      expect(newQueue.dequeue()).toBe(1);
+      expect(newQueue.dequeue()).toBe(3);
+      console.log(newQueue);
+      expect(newQueue.dequeue()).toBe(undefined);
+      done();
+
+    });
 
   });
 
-    it('should return first item', (done) => {
+    it('should return first item', (undefined) => {
       let firstSlot = newQueue.dequeue();
 
       expect(firstSlot).toEqual(1);
-      expect(newQueue).toEqual({"queue" : [2,3]});
+      expect(newQueue).toEqual({"queue": [2,3]});
       done();
 
     });

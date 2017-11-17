614b5df365ccbbd6764b7767a7bf188fe8c5f911
dalhun namgung <bnamgung@gmail.com>
2017-11-16 19:28:54 -0800

    will finish

--- test/queue.test.js
+++ test/queue.test.js
@@ -1,25 +1,28 @@
 'use strict';
 
 
-const Queue = require('../lib/queue');
-const expect = require('expect');
+const Queue = require('../lib/queue/');
 
 
 describe('queue testing', () => {
 
-  let newQueue = new Queue();
-
   it('should return 0', () => {
+    let newQueue = new Queue();
+    
     expect(newQueue.length).toBe(null);
   });
 
+
   it('should return 1:3, length:2, head:null, null:1', () => {
     let newQueue = new Queue();
-    newQueue.enqueue(1),
-    newQueue.enqueue(3),
-    expect(newQueue.length).toBe(2),
-    expect(newQueue[1].toBe(3)
+
+    newQueue.enqueue(1);
+    newQueue.enqueue(3);
+    expect(newQueue.length).toBe(2);
+    expect(newQueue.enqueue[1].toBe(3);
+
   });
+
 
   it('should return length:2, head:3', () => {
     let newQueue = new Queue();
@@ -30,5 +33,6 @@ describe('queue testing', () => {
     expect(newQueue.dequeue()).toBe(3);
     console.log(newQueue);
     expect(newQueue.dequeue()).toBe(undefined);
+
   });
 });

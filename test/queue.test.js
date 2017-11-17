411a50ec18eb6cf953e66e5de9a78175fd7cbb37
dalhun namgung <bnamgung@gmail.com>
2017-11-17 13:24:35 -0800

    modify

--- test/queue.test.js
+++ test/queue.test.js
@@ -1,15 +1,17 @@
 'use strict';
 
 
-const Queue = require('../lib/queue/');
+
+const Queue = require('../lib/queue');
 
 
 describe('queue testing', () => {
 
   it('should return 0', () => {
     let newQueue = new Queue();
-    
+
     expect(newQueue.length).toBe(null);
+
   });
 
 
@@ -19,6 +21,7 @@ describe('queue testing', () => {
     newQueue.enqueue(1);
     newQueue.enqueue(3);
     expect(newQueue.length).toBe(2);
+      console.log(newQueue.length);
     expect(newQueue.enqueue[1].toBe(3);
 
   });
@@ -31,8 +34,13 @@ describe('queue testing', () => {
     newQueue.enqueue(3);
     expect(newQueue.dequeue()).toBe(1);
     expect(newQueue.dequeue()).toBe(3);
-    console.log(newQueue);
+      console.log(newQueue);
     expect(newQueue.dequeue()).toBe(undefined);
 
   });
 });
+
+
+
+/*
+Similarly, a queue data structure follows a "First In First Out" policy (as in the case of a normal queue when we stand in line at the counter), where the first element is pushed into the queue or "Enqueued" and the same element when it has to be removed from the queue is "Dequeued"..

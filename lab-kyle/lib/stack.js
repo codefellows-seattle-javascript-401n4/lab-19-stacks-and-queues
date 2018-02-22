b5f96fb6feb88f416272514bde9ac26c3998e905
Kyle Richey <richey.kyle.i@gmail.com>
2018-02-21 18:49:05 -0800

    done easiest lab ever

---
+++ lab-kyle/lib/stack.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = class Stack {
+  constructor() {
+    this.stack = new Array();
+  }
+
+  push(value) {
+    this.stack.push(value);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+};
